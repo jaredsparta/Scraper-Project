f41076624e46b1c8d2a78e523f42352397f86f49
jaredsparta <73166572+jaredsparta@users.noreply.github.com>
2020-12-15 21:28:29 +0000

    more setup

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,5 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/focal64"
-
+  config.vm.network "private_network", ip: "192.168.10.100"
+  config.vm.provision "shell", path: "setup-files/provision-ansible.sh"
 end
--- setup-files/provision-ansible.sh
+++ setup-files/provision-ansible.sh
@@ -5,6 +5,5 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible -y
 
-# Will rename the default hosts file to hosts.default and set a symlink to the one in our repo
-sudo cp /etc/ansible/hosts /etc/ansible/hosts.default
-sudo ln -s /home/ubuntu/Ansible-1/setup-files/hosts /etc/ansible/hosts
+cd ~/
+git clone https://github.com/jaredsparta/Scraper-Project.git
