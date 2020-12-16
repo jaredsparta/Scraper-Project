0952df51ed37786c7832971cfde0febca3ca1f7f
jaredsparta <73166572+jaredsparta@users.noreply.github.com>
2020-12-16 15:29:33 +0000

    test

--- README.md
+++ README.md
@@ -6,5 +6,11 @@
 <br>
 
 ## Dev Environments
+- We can create a development environment using Vagrant boxes. As Ansible does not work on Windows machines, we must use a virtual box with an Ubuntu box to do so.
+    - The `Vagrantfile` included will be able to be used to do so.
 
-- 
+- The `.venv` included within the repository is a virtual environment that can be used. What was installed:
+    1. `python3 -m pip install bs4`
+    2. `python3 -m pip install requests`
+    3. `python3 -m pip install pytest`
+    4. `python3 -m pip install requirements.txt`
--- setup-files/provision-ansible.sh
+++ setup-files/provision-ansible.sh
@@ -4,6 +4,9 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible -y
 
+# Installing venv support
+sudo apt-get install python3-venv
+
 # Installing pip3 and app dependencies
 # sudo apt-get install python-pip -y
 # sudo apt-get install python-bs4 -y
@@ -14,13 +17,12 @@ sudo apt-get install ansible -y
 # pip3 install pytest
 
 # Making a directory for the files to be downloaded to
-mkdir ~/Downloads
-chmod 777 ~/Downloads
+# mkdir ~/Downloads
+# chmod 777 ~/Downloads
 
 # Installing pip
-sudo apt-get install python3-pip -y
-sudo apt-get install python3-venv -y
+# sudo apt-get install python3-pip -y
+# sudo apt-get install python3-venv -y
 
 # python3 -m venv venv
 # pip install pytest, bs4, requests
-#
